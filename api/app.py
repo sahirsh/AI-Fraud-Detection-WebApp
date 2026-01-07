957de21484452981019754773e59d3f58dcb4089
Yulitz <sahirh@uci.edu>
2026-01-06 21:37:17 -0800

    cors

--- api/app.py
+++ api/app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
 from api.inference  import predict_job_fraud
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/health', methods=["GET"])
 def health():
